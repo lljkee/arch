2c2aaf64187cbea6c944a6c351b5a84f8550b4a3
lljk <npfpwr@gmail.com>
2019-12-24 13:19:02 +0300

    Update and rename arch1.sh to lljk.sh
    lljk

--- lljk.sh
+++ lljk.sh
@@ -75,4 +75,4 @@ pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl grub wget
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL https://github.com/lljkee/arch/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL https://git.io/JeFSV)"
