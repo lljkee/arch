ae67f92aa759a60d0b0f71d67ae80dbe8b3243c1
lljk <npfpwr@gmail.com>
2020-02-01 19:12:36 +0300

    Update aurdim.sh

--- aurdim.sh
+++ aurdim.sh
@@ -4,4 +4,4 @@ wget git.io/yay-dim.sh && sh yay-dim.sh --noconfirm
 yay -S nomachine vertex-themes xcursor-breeze-serie-obsidian breeze-obsidian-cursor-theme bluez-hciconfig bluez-hcitool --noconfirm
 
 
-#wget git.io/aurdim.sh
+#wget git.io/aurdim.sh && sh aurdim.sh
