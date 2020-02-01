fde9e3b769f6970dfcdf0d63e6ce258b4352691c
lljk <npfpwr@gmail.com>
2020-02-01 19:04:50 +0300

    Update aurdim.sh

--- aurdim.sh
+++ aurdim.sh
@@ -2,3 +2,6 @@
 echo 'Установка AUR (yay)'
 wget git.io/yay-dim.sh && sh yay-dim.sh --noconfirm
 yay -S nomachine vertex-themes xcursor-breeze-serie-obsidian breeze-obsidian-cursor-theme bluez-hciconfig bluez-hcitool
+
+
+#wget git.io/aurdim.sh
