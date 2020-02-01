fe7a9ffe18158f2ac75c3a9b16314bdec41d4625
lljk <npfpwr@gmail.com>
2020-02-01 17:38:15 +0300

    Update vboxinstall.sh

--- vboxinstall.sh
+++ vboxinstall.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-
-echo '2.4.3 Монтирование дисков'
-mount /dev/sda2 /mnt
-mkdir /mnt/{boot,home}
-mount /dev/sda1 /mnt/boot
-swapon /dev/sda3
-mount /dev/sda4 /mnt/home
-
-echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
-echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-
-echo '3.1.1 Обновляем ключи'
-pacman-key --init && pacman-key --populate archlinux && pacman-key --refresh-keys
-
-echo '3.2 Установка основных пакетов'
-pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl grub wget
-
-echo '3.3 Настройка системы'
-genfstab -pU /mnt >> /mnt/etc/fstab
-
-arch-chroot /mnt sh -c "$(curl -fsSL https://git.io/dimxfce1.sh)"
+#git.io/vbxtest.sh
